8c54d0447c97d99feed835448b337e97a4b7c7a3
patel <paste-the-generated-address>
2026-08-20 11:03:05 +0530

    Add Screen doctype

---
+++ theatre_booking/theatre_booking/doctype/screen/screen.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, patel and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Screen(Document):
+	pass
